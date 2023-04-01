2bba8b2da451242c36940aff51d0faecc58780e8
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-04-01 14:27:20 +0900

    commit

--- UIImageView/ViewController.swift
+++ UIImageView/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import Kingfisher
+
+struct Pokemon: Encodable {
+    let name: String
+    let url: URL
+}
 
 class ViewController: UIViewController {
+    let pokemons: [Pokemon] = [
+        Pokemon(name: "フシギバナ", url: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png")!),
+        Pokemon(name: "リザードン", url: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png")!),
+        Pokemon(name: "カメックス", url: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png")!)
+    ]
 
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var pokemonName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        randomChange()
     }
 
-
+    @IBAction func changePokemon(_ sender: Any) {
+        randomChange()
+    }
+    
+    private func randomChange() {
+        
+        let index = Int.random(in: 0...2)
+        pokemonName.text = pokemons[index].name
+        pokemonImage.kf.setImage(with: pokemons[index].url)
+//        if let imageData = try? Data(contentsOf: pokemons[index].url) {
+//            pokemonImage.image = UIImage(data: imageData)
+//        }
+    }
 }
 
